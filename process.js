13aba5ac9991f40ab3b27a5b3698465dba0f7e91
Pulan <ekgus5095@gmail.com>
2022-07-21 12:43:44 +0900

    Initial process setup

---
+++ process.js
@@ -0,0 +1,24 @@
+// TODO - 페이지 접근시 hero를 가운데로 위치
+//? localStorage에 hero의 처음 위치를 지정해서 렌더링해면 어떤가?
+//? 새로고침/페이지 접근/GaveOver시에 render함수를 만들어서 실행시키는건?
+
+//* - hero를 가운데에서 시작하기
+
+// TODO - hero를 좌우로 움직이게하기
+//? content에 relative줘서 hero를 left, right로 움직이게 하기
+//* - window에 eventListener로 움직이기 (keyDown)
+//* - 정지시 hero는 앞을 보고 시작함 (keyUp)
+//* - 바탕화면 끝에 도달하면 정지 -> hero를 앞으로 보게하기
+
+// TODO - 무작위로 귀신이 하늘에서 내려오기
+//* - 시작 버튼 클릭 -> 랜덤한 x의 위체에서 귀신 생성
+//* - -> 귀신 내려오게하기
+
+// TODO - Gave Over
+//* - 귀신과 hero가 접촉하게 될시 진행 중이던 유닛들을 멈추게함
+//* - alert 창으로 게임이 종료됬다고 표시
+//* - 확인시 화면을 초기화면으로 위치
+
+//~ 참고 문서
+//* alert창 이후 이벤트 주기
+//* https://stackoverflow.com/questions/9394131/go-to-url-after-ok-button-if-alert-is-pressed/9394143#9394143
